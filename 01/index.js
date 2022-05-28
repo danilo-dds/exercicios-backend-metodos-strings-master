94371f342acf1f809ef80598c66428edfa90cda7
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-28 15:48:57 -0300

    exercicio 01 feito

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,10 @@
 const comentario = "Esse COVID é muito perigoso!";
+const achouCovid = comentario.toUpperCase().includes("COVID")
+const achouPandemia = comentario.toUpperCase().includes("PANDEMIA")
+
+if (achouCovid || achouPandemia) {
+    console.log("Comentário bloqueado por conter palavras proibidas")
+} else {
+    console.log("Comentário autorizado.")
+}
+//pandemia ou covid
