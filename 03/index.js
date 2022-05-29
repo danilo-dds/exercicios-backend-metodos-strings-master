1fb56109b71e87ba62faf2754f191c81cf60f6aa
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-29 10:05:10 -0300

    exercicio 04 feito

--- 03/index.js
+++ 03/index.js
@@ -1,7 +1,7 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
 let urlFormatada = ""
 for (let espaço of texto) {
-    urlFormatada += espaço.toUpperCase().replace(" ", "-")
+    urlFormatada += espaço.toLowerCase().replace(" ", "-")
 }
 
 console.log(urlFormatada);
--- 04/index.js
+++ 04/index.js
@@ -1,3 +1,15 @@
 let identificador = "123";
 let nome = "José silva costa";
-let email = "      jose@email.com  ";
+let email = "      jose@email.com   ";
+
+identificador = identificador.padStart(6, "0")
+let nomeMaiusculo = ""
+
+for (const itens of nome.split(" ")) {
+    nomeMaiusculo += `${itens.slice(0, 1).toUpperCase()}${itens.slice(1).toLowerCase()} `
+}
+nome = nomeMaiusculo.trim()
+email = email.trim()
+console.log(identificador);
+console.log(nome);
+console.log(email);
