bcaba8e03009273bbc5b38b6173fe91d44528249
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-29 17:27:41 -0300

    exercicio 07 feito

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,11 @@
-const email = "aluno@cubos.academy";
+const email = "jose@cubos.academy.br";
+
+function validarEmail(email) {
+    if (email[0] === "." || email[email.length - 1] === "." || email.includes("@") === false || email.includes(".") === false) {
+        console.log("E-mail inválido")
+    } else {
+        console.log("E-mail válido")
+    }
+}
+
+validarEmail(email);
