d175f1a7a1c43c531303a54266fedb6808c5d117
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-29 10:27:58 -0300

    exercicio 05 feito

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,5 @@
 const numeroCartao = '1111222233334444';
+
+let numeroCartaoFormatado = `${numeroCartao.slice(0, 4).padEnd(12, "*",)}${numeroCartao.slice(12)}`
+
+console.log(numeroCartaoFormatado);
