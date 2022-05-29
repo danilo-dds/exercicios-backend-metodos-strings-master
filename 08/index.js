166f09dec6bced5db2cc01c260e6f12177c7edc2
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-29 17:49:32 -0300

    exercicio 08 feito

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,15 @@
 const cpf = "011.022.033-44";
+const cnpj = "11.022.033/0001-44";
+
+
+function removerPontuacao(numero) {
+    let numeros = numero.replace('-', '').replace('/', '').split('.');
+    let numeroLimpo = ""
+    for (let iten of numeros) {
+        numeroLimpo += iten;
+    }
+    console.log(numeroLimpo);
+}
+
+removerPontuacao(cpf);
+removerPontuacao(cnpj);
