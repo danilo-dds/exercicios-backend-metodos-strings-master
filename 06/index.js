3237f9f3b31c664a7665f3a74464b66308393736
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-29 16:56:02 -0300

    exercicio 06 feito

--- 06/index.js
+++ 06/index.js
@@ -1,1 +1,22 @@
-const celular = 7199918888;
+const celular = 73999918888;
+
+function formatarCelular(numero) {
+    let celular = "";
+    if (numero.length === 8) {
+        celular = `9 ${numero.slice(0, 4)}-${numero.slice(4)}`;
+        console.log(celular);
+    } else if (numero.length === 9) {
+        celular = `${numero.slice(0, 1)} ${numero.slice(1, 5)}-${numero.slice(5)}`;
+        console.log(celular);
+    } else if (numero.length === 10) {
+        celular = `(${numero.slice(0, 2)}) 9 ${numero.slice(2, 6)}-${numero.slice(6)}`;
+        console.log(celular);
+    } else if (numero.length === 11) {
+        celular = `(${numero.slice(0, 2)}) ${numero.slice(2, 3)} ${numero.slice(3, 7)}-${numero.slice(7)}`;
+        console.log(celular);
+    } else {
+        console.log("número invalido.")
+    }
+}
+
+formatarCelular(celular.toString())
