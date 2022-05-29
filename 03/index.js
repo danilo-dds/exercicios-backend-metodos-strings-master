53a95e3eac62f45db83e808c65a7f4e7cdaa15f1
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-29 09:18:27 -0300

    exercicio 03 feito

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,7 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+let urlFormatada = ""
+for (let espaço of texto) {
+    urlFormatada += espaço.toUpperCase().replace(" ", "-")
+}
+
+console.log(urlFormatada);
