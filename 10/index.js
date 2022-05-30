6be854bf38d16f97758c6ab60bd08f9d34a7485b
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-30 11:12:59 -0300

    exercicio 10 feito

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,11 @@
-const nomeArquivo = 'Foto da Familia.pdf';
+const nomeArquivo = 'Foto da Familia.jpeg';
+function validarArquivo(arquivo) {
+    const extensao = arquivo.slice(arquivo.lastIndexOf('.') + 1);
+    if (extensao === "jpg" || extensao === "jpeg" || extensao === "gif" || extensao === "png") {
+        console.log("Arquivo válido")
+    } else {
+        console.log("Arquivo inválido")
+    }
+}
+
+validarArquivo(nomeArquivo);
