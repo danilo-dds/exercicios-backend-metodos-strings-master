048888fb390e204516056a8ec65f5f4153a461cd
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-30 08:55:05 -0300

    exercicio 09 feito

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,11 @@
 const nome = 'Guido Cerqueira';
+function geradorNickname(nome) {
+    const nomes = nome.trim().toLowerCase().split(' ');
+    let nomeSemEspacos = "";
+    for (const nome of nomes) {
+        nomeSemEspacos += nome;
+    }
+    console.log(`@${nomeSemEspacos.slice(0, 12)}`);
+
+}
+geradorNickname(nome);
